82057fe87b27c1381ab1eb0807c6143397e35c0b
Rishabh <rishabh.sharma3@appinventiv.com>
2025-05-27 18:22:57 +0530

    Added modular css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import { useDispatch, useSelector } from "react-redux"
-import { deposit, setAmount, withdraw } from "./features/bank/bankSlice"
-import type { RootState } from "./store/store"
-
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { deposit, withdraw, setAmount } from "./features/bank/bankSlice";
+import type { RootState } from "./store/store";
 const App = () => {
-  const dispatch = useDispatch()
-  const {amount, balance} = useSelector((state: RootState) => state.bank)
+  const { balance, amount } = useSelector((state: RootState) => state.bank);
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Bank</h1>
-      <h2>Balance: {balance}</h2>
-      <input type="number" placeholder="Enter amount" value={amount} onChange={(e) => dispatch(setAmount(e.target.value))} />
-      <button onClick={() => dispatch(deposit())}>Deposit</button>
-      <button onClick={() => dispatch(withdraw())}>Withdraw</button>
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>🏦 My Bank</h1>
+        <p className={styles.balance}>Balance: ₹{balance.toFixed(2)}</p>
+        <input
+          className={styles.input}
+          type="number"
+          value={amount}
+          placeholder="Enter amount"
+          onChange={(e) => dispatch(setAmount(Number(e.target.value)))}
+        />
+        <div className={styles.buttons}>
+          <button className={styles.depositBtn} onClick={() => dispatch(deposit())}>
+            Deposit
+          </button>
+          <button className={styles.withdrawBtn} onClick={() => dispatch(withdraw())}>
+            Withdraw
+          </button>
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
